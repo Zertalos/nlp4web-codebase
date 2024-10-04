240decd70d4310e8b41199e09f57e9b081a55fa3
kwang2049 <kexin.wang.2049@gmail.com>
2024-10-04 22:04:20 +0200

    init

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name="nlp4web",
+    version="0.0.0",
+    author="Kexin Wang",
+    author_email="kexin.wang.2049@gmail.com",
+    description="Codebase of teaching materials for NLP4Web.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/kwang2049/nlp4web",
+    project_urls={
+        "Bug Tracker": "https://github.com/kwang2049/nlp4web/issues",
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "nltk==3.9.1",
+        "numpy==2.1.1",
+        "pandas==2.2.3",
+        "tqdm==4.66.5",
+        "ujson==5.10.0",
+        "joblib==1.4.2",
+        "datasets==3.0.1"
+    ],
+)
